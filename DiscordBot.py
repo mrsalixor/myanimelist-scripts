afe01f1b57175fcfb5bc047e4884eebe9aba10f7
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-06-11 11:24:29 +0200

    First version of the Discord bot finished

--- DiscordBot.py
+++ DiscordBot.py
@@ -22,86 +22,69 @@ async def on_message(message):
     if message.content.lower().startswith('!anime_stats') or message.content.lower().startswith('!manga_stats'):
         if message.content.lower().startswith('!anime_stats'):
             worktype = 'anime'
-        elif message.content.lower().startswith('!anime_stats'):
+        elif message.content.lower().startswith('!manga_stats'):
             worktype = 'manga'
         else:
             return
 
-        search_id = int(message.content.split(" ")[1])
-        # print(search_id, flush=True)
-        if not search_id:
-            await client.send_message(message.channel, 'Please type !{}_stats <id>'.format(worktype))
+        message_split = message.content.split(" ")
+
+        if len(message_split) <= 1:
+            await client.send_message(message.channel, 'Please type `!{}_stats <id>`.'.format(worktype))
         else:
-            url = 'http://cocchi.iiens.net/MALUpdater/shared_works_' + worktype + '.csv'
-            req = urllib.request.urlopen(url)
+            search_id = message_split[1]
 
-            csvfile = csv.reader(codecs.iterdecode(req, 'utf-8'), delimiter='|')
-            line_match = []
-
-            for index, line in enumerate(csvfile):
-                if index == 0:
-                    pseudos = [pseudo for pseudo in line]
-                else:
-                    id = int(line[0])
-                    if id == search_id:
-                        line_match = line
-                        break
-
-            if line_match != []:
-                title = line_match[1]
-                poster_url = line_match[3]
-                mal_url = "https://myanimelist.net/{}/{}".format(worktype, search_id)
-
-                # result = discord.Embed(title=title, description=description, url=mal_url)
-                result = discord.Embed(title=title, url=mal_url)
-                result.set_footer(text="Test", icon_url="https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png")
-                result.set_thumbnail(url=poster_url)
-
-                statuses_scores = [s for s in line_match[4:4+len(pseudos)]]
-                description = ""
-
-                for index, pseudo in enumerate(pseudos):
-                    description += pseudo + " - "
-                    if statuses_scores[index] == 'O':
-                        result.add_field(name=pseudo, value="On-hold")
-                        description += "On-hold"
-                    elif statuses_scores[index] == 'D':
-                        result.add_field(name=pseudo, value="Dropped")
-                        description += "Dropped"
-                    elif statuses_scores[index] == 'P' and worktype == "anime":
-                        result.add_field(name=pseudo, value="Plan to watch")
-                        description += "Plan to watch"
-                    elif statuses_scores[index] == 'P' and worktype == "manga":
-                        result.add_field(name=pseudo, value="Plan to read")
-                        description += "Plan to read"
-                    elif statuses_scores[index].isnumeric():
-                        if int(statuses_scores[index]) == 0:
-                            result.add_field(name=pseudo, value="Not yet rated")
-                            description += "Not yet rated"
-                        else:
-                            result.add_field(name=pseudo, value="Score : " + statuses_scores[index])
-                            description += "Score : " + statuses_scores[index]
-                    else:
-                        result.add_field(name=pseudo, value="Not in list")
-                        description += "Not in list"
-                    description += "\n"
-
-                await client.send_message(message.channel, embed=result)
+            if not search_id.isnumeric():
+                await client.send_message(message.channel, 'Please type `!{}_stats <id>`.'.format(worktype))
             else:
-                await client.send_message(message.channel, 'No such anime was found')
+                search_id = int(search_id)
 
+                url = 'http://cocchi.iiens.net/MALUpdater/shared_works_' + worktype + '.csv'
+                req = urllib.request.urlopen(url)
 
+                csvfile = csv.reader(codecs.iterdecode(req, 'utf-8'), delimiter='|')
+                line_match = []
 
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+                for index, line in enumerate(csvfile):
+                    if index == 0:
+                        pseudos = [pseudo for pseudo in line]
+                    else:
+                        id = int(line[0])
+                        if id == search_id:
+                            line_match = line
+                            break
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+                if line_match != []:
+                    title = line_match[1]
+                    poster_url = line_match[3]
+                    mal_url = "https://myanimelist.net/{}/{}".format(worktype, search_id)
+
+                    result = discord.Embed(title=title, url=mal_url, color=0xf7ed3b)
+                    result.set_footer(text=worktype.capitalize(), icon_url="https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png")
+                    result.set_thumbnail(url=poster_url)
+
+                    statuses_scores = [s for s in line_match[4:4+len(pseudos)]]
+
+                    for index, pseudo in enumerate(pseudos):
+                        if statuses_scores[index] == 'O':
+                            result.add_field(name=pseudo, value="On-hold")
+                        elif statuses_scores[index] == 'D':
+                            result.add_field(name=pseudo, value="Dropped")
+                        elif statuses_scores[index] == 'P' and worktype == "anime":
+                            result.add_field(name=pseudo, value="Plan to watch")
+                        elif statuses_scores[index] == 'P' and worktype == "manga":
+                            result.add_field(name=pseudo, value="Plan to read")
+                        elif statuses_scores[index].isnumeric():
+                            if int(statuses_scores[index]) == 0:
+                                result.add_field(name=pseudo, value="Not yet rated")
+                            else:
+                                result.add_field(name=pseudo, value="Score : " + statuses_scores[index])
+                        else:
+                            result.add_field(name=pseudo, value="Not in list")
+
+                    await client.send_message(message.channel, embed=result)
+                else:
+                    await client.send_message(message.channel, 'No such anime was found.')
+
 
 client.run('MzIzMjExNTQzNzQxNDY0NTc4.DB34RA.bdVDgEe9KXHKiBhwoRoYtojfHGY')
